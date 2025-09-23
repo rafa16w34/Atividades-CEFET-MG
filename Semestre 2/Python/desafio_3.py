dd561e5316f07e769e0f209e7fb1dc5eb28ec621
Rafael Alves <rafael16alvesf@gmail.com>
2025-09-23 08:24:10 -0300

    Atualização

--- Semestre 2/Python/desafio_3.py
+++ Semestre 2/Python/desafio_3.py
@@ -1,3 +1,5 @@
+print(' ') #Espaço de separação
+
 #Frase
 frase = str(input('Digite uma frase:'))
 
@@ -5,54 +7,15 @@ frase.lower()
 
 lista_da_frase = []
 
+print(' ') #Espaço de separação
+
 #Lista e número de palavras
 
 palavras = frase.split()
 
-print(f'O número de palavras da frase é: {len(palavras)}')
+print(f'Total de palavras únicas: {len(palavras)}')
 
-#Lista de vogais
-
-lista_vogal = []
-
-vogais = ['a','e','i','o','u']
-
-for i in range(len(palavras)):
-
-    comeca_com_vogal = False
-
-    for j in range(len(vogais)):
-
-        comeca_com_vogal = palavras[i].startswith(vogais[j])
-        
-        if comeca_com_vogal:
-            lista_vogal.append(palavras[i])
-            break
-
-
-#Inverter palavras
-
-palavras_invertidas = []
-
-for i in range(len(palavras)):
-
-    palavras_invertidas.append(palavras[i][::-1])
-
-print(palavras)
-print(palavras_invertidas)
-
-
-#Media de letras de cada palavra
-
-media = 0.0
-
-for i in range(len(palavras)):
-
-    media += len(palavras[i])
-
-media = media/len(palavras)
-
-print(f'A média de letras da frase é : {media}')
+print(' ') #Espaço de separação
 
 #Maior e menor palavra
 
@@ -73,9 +36,59 @@ for i in range (len(palavras)):
 
 
 
-print(f' Maior palavra "{maior}" e a menor "{menor}"')
+print(f'Mais longa: "{maior}" | Mais curta: "{menor}"')
+
+print(' ') #Espaço de separação
+
+#Lista de vogais
+
+lista_vogal = []
+
+vogais = ['a','e','i','o','u']
+
+for i in range(len(palavras)):
+
+    comeca_com_vogal = False
+
+    for j in range(len(vogais)):
+
+        comeca_com_vogal = palavras[i].startswith(vogais[j])
+        
+        if comeca_com_vogal:
+            lista_vogal.append(palavras[i])
+            break
 
 
+print(f'Palavras que começam com vogal: {lista_vogal}')
+
+print(' ') #Espaço de separação
+
+#Inverter palavras
+
+palavras_invertidas = []
+
+for i in range(len(palavras)):
+
+    palavras_invertidas.append(palavras[i][::-1])
+
+
+print(f'Frase invertida: "{frase[::-1]}"')
+
+print(' ') #Espaço de separação
+
+#Media de letras de cada palavra
+
+media = 0.0
+
+for i in range(len(palavras)):
+
+    media += len(palavras[i])
+
+media = media/len(palavras)
+
+print(f'Média de caracteres por palavra: {media}')
+
+print(' ') #Espaço de separação
 
 #Ordem alfabética inversa
 
@@ -94,5 +107,6 @@ for i in range(len(alfabeto)):
 
 
 
-print(ordem_alfabetica[::-1])
+print(f'Palavras em ordem alfabética inversa:{ordem_alfabetica[::-1]}')
 
+print(' ') #Espaço de separação
