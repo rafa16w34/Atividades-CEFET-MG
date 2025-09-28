b33d97d0544bde0e57fcba7a293f991963883577
Rafael Alves <rafael16alvesf@gmail.com>
2025-09-28 16:40:25 -0300

    Atualização

--- Semestre 2/Python/09.26.25.py
+++ Semestre 2/Python/09.26.25.py
@@ -1,5 +1,5 @@
 #Exercício 1
-
+'''
 from math import sqrt, pow
 
 n = []
@@ -41,10 +41,26 @@ for i in range(quantidade):
     lista_2.add( int(input(f'Digite o número {i} da lista 2 que você deseja: ')))
 
 print(f'A intercessão entre as duas listas é: {lista_1&lista_2}')
+''''''
+
 
 #Exercicio 3
 
-sequencia_1 = list(map(int,input('Digite os números da 1º sequência, separados por espaço: ')split))
+sequencia_1 = set([])
+quantidade = int(input('\nQuantos números gostaria de digitar para a sequência 1?\n'))
+
+for i in range(quantidade):
+    sequencia_1.add( int(input(f'Digite o número {i} da lista 1 que você deseja: ')))
+
+print(sequencia_1)
+
+sequencia_2 = set([])
+quantidade = int(input('\nQuantos números gostaria de digitar para a lista 2?\n'))
+
+for i in range(quantidade):
+    sequencia_2.add( map(int(input(f'Digite o número {i} da lista 2 que você deseja: '))))
+
+print(sequencia_2)
 
 
 
@@ -53,8 +69,35 @@ sequencia_1 = list(map(int,input('Digite os números da 1º sequência, separado
 
 sequencia_organizada = sequencia_1 + sequencia_2
 
-print(sequencia_organizada)
 
-sequencia_organizada.sort()
 
 print(sequencia_organizada)
+
+sequencia_organizada = sorted(sequencia_organizada)
+
+print(sequencia_organizada)'''
+
+#Exercicio 4
+
+'''escolha = str(input('Digite qual média você quer escolher:\n1 - Aritmética\n2 - Ponderada\n3 - Harmônica'))
+
+lista = []
+
+for i in range(3):
+    lista.append( int(input(f'Digite a nota {i}: ')))
+
+
+
+match (escolha):
+
+    case 1:
+        for i in range(3):
+            soma = soma + lista[i]
+        media = soma/3
+        print(f'A média é {media}')
+
+    case 2:
+        for i in range(3):
+                soma = soma + (lista[i] *3)
+        media = soma/3
+        print(f'A média é {media}')'''
